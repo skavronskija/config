50c257ec6de46b46f159e2477d8ba073bc946bae
skavronskija <toxa@ubuntu.sp.wsl>
2019-07-18 14:29:43 +0100

    bashrc

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,7 +12,7 @@ function doIt() {
 		--exclude "README.md" \
 		--exclude "LICENSE-MIT.txt" \
 		-avh --no-perms . ~;
-	source ~/.bashrc;
+	#source ~/.bashrc;
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
